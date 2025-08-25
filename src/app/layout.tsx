32af9ebfd4477592d2aeecd0562c1ab7ec053fb8
Daniel427217 <danielcandelariosaldarriaga@gmail.com>
2025-08-25 14:53:08 -0500

    Realización de componente (Sidebar)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+        <head>
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        </head>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          {children}
+        </body>
+      </html>
   );
 }
